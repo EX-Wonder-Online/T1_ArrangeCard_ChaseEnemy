417958a4f4a54a3935374fe68f69dcfa4f94e86e
TakaoWing <tt.wing2838783@gmail.com>
2020-05-22 13:31:44 +0900

    エネミーがターゲットに向かうプログラムを追加

---
+++ Assets/Scripts/EnemyMove.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI; // AIを追加　NavMeshAgentを使えるように入れる
+
+public class EnemyMove : MonoBehaviour
+{
+    [SerializeField]
+    private NavMeshAgent aganet; // Agentを取得
+    [SerializeField]
+    private GameObject terget; // 目的地を指定　Playerにすると鬼ごっこできる
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        aganet.SetDestination(terget.transform.position); // 常に自分自身はプレイヤーに向かう
+    }
+}
